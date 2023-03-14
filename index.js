d36b3d62b547ad47ddd22ff375f47e597b88dcb9
mary_waiyego <waiyegomary55@gmail.com>
2023-03-14 13:29:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks (value) {
+    return 1;
+}
+function distanceFromHqInBlocks (value) {
+    return 8;
+}
+
+function distanceFromHqInFeet (value) {
+    return 264;
+}
+function distanceFromHqInFeet (value) {
+    return 2112;
+}
+function distanceTravelledInFeet (value) {
+    return 1320;
+}
+function distanceTravelledInFeet (value) {
+    return 1584;
+}
+function calculatesFarePrice (value) {
+    return 0;
+}
+function calculatesFarePrice (value) {
+    return 2.56;
+}
+function calculatesFarePrice (value) {
+    return 25;
+}
+function calculatesFarePrice (value) {
+    return 'cannot travel that far';
+}
